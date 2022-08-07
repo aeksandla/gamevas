e0a547cd52d1dbdaa9e74a5e9ff7bbbe71a98d8b
aleksandra <sasha.pleshyova@yandex.ru>
2022-08-07 11:00:26 +0300

    Remove Cell component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
-export * from './Cell';
 export * from './Canvas';
-export * from './CellsField';
 export * from './Game';
 export * from './GameObject';
 export * from './Man';
--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,4 @@ export const ORIGIN_PARAMS = {
     canvasHeight: 1020,
     gridCellWidth: 20,
     gridCellHeight: 20,
-    cellWidth: 50,
-    cellHeight: 50,
 };
-
-export const BACKGROUND = '#b7f2ff';
